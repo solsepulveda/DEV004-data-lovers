0cacd7d823242b314903fa1da1d93f4e5e0226ad
barbvilla <barbara.villasenor.c@gmail.com>
2023-02-04 22:42:46 -0300

    funciones titulo y poster

--- src/data.js
+++ src/data.js
@@ -1,4 +1,38 @@
 // estas funciones son de ejemplo
+const funciones = {
+  title : title,
+  img : img
+};
+
+function title (anything) {
+  for (const movie of anything) {
+    const movieTitle = movie.title;    
+    return movieTitle
+  }  
+}
+
+
+function img (anything) {
+  for (const imagen of anything) {
+    const movieImg = imagen.poster;    
+    return movieImg    
+  }
+}
+
+
+
+
+
+
+/* function movieList(anything){  
+  const movies = Object.entries(anything);
+  // console.log(movies);
+  return movies;
+} */
+
+//function movieNames(anything)
+
+
 
 export const example = () => {
   return 'example';
@@ -7,3 +41,5 @@ export const example = () => {
 export const anotherExample = () => {
   return 'OMG';
 };
+
+export default funciones;
--- src/main.js
+++ src/main.js
@@ -1,6 +1,22 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import funciones from './data.js';
+import data from './data/ghibli/ghibli.js';
 
-console.log(example, data);
+/* const movies = data.films;
+const title = funciones.movieList(movies); */
+
+const movies = data.films;
+const root = document.getElementById("root");
+
+const titulo = funciones.title(movies);
+const movieDiv = document.createElement("div");
+movieDiv.innerHTML = titulo; 
+root.appendChild(movieDiv);
+
+
+const imagen = funciones.img(movies);
+const otroDiv = document.createElement("img");    
+otroDiv.src = imagen;
+root.appendChild(otroDiv);
+
+
+// console.log(title);
