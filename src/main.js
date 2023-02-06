a7b4f75e12a7ea85cf62c4543becb363f09bbedf
solsepulveda <solangesepulved@gmail.com>
2023-02-06 19:41:36 -0300

    despliegue de título y poster

--- src/main.js
+++ src/main.js
@@ -1,6 +1,30 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import ghibli from "./data/ghibli/ghibli.js";
 
-console.log(example, data);
+const movies = ghibli.films;
+const root = document.getElementById("root");
+
+const deploy = (movies) => {
+  for (const movie of movies) {
+    /* const movieTitle = movie.title; */
+    const movieDiv = document.createElement("div");
+    movieDiv.classList.add("card"); //clase para editar en css*/
+    //con movie.title llamamos a la constante y le asignamos una etiqueta//
+    movieDiv.innerHTML += `<h2>${movie.title}</h2>
+    <img src="${movie.poster}" alt="imágen de la película">`//siempre poner en imágenes alt
+    root.appendChild(movieDiv);
+  }
+};
+deploy(movies);
+
+
+/* const img = (movies) => {
+  for (const imagen of movies) {
+    const movieImg = imagen.poster;
+    const otroDiv = document.createElement("img");
+    otroDiv.src = movieImg;
+    root.appendChild(otroDiv);
+  }
+};
+img(movies); */
+
+console.log(ghibli);
