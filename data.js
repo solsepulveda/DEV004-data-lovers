63356c17f7435ddcf97462d1757d65feab5f2764
solsepulveda <solangesepulved@gmail.com>
2023-02-28 20:29:03 -0300

    Updates

--- data.js
+++ data.js
@@ -1,31 +1,39 @@
-
-export const alphabeticOrderAsc = function (anyArray) {
-  const orderAZ = anyArray.sort((a, b) => (a.title > b.title) ? 1 : (a.title < b.title) ? -1 : 0);
+export const alphabeticOrderAsc = function (movies) {
+  const orderAZ = movies.sort((a, b) => (a.title > b.title) ? 1 : (a.title < b.title) ? -1 : 0);
   return orderAZ;  
 };
 
-export const alphabeticOrderDes = function (anyArray) {
-  const orderZA = anyArray.sort((a, b) => (a.title > b.title) ? -1 : (a.title < b.title) ? 1 : 0);
+export const alphabeticOrderDes = function (movies) {
+  const orderZA = movies.sort((a, b) => (a.title > b.title) ? -1 : (a.title < b.title) ? 1 : 0);
   return orderZA;
 };
 
-export const mappedDirector = function(anyArray) {
-  const director = anyArray.map(anyArray => anyArray.director)
-  return director;
+// extrae nombre de directores
+export const mappedDirector = function(movies) {
+  const director = movies.map(anyArray => anyArray.director)
+  return director
 }
 
-export const filteredDirector = function(anyArray) {
-  const onlyDirectorName = anyArray.filter((director, index) => {
-    return anyArray.indexOf(director) === index
+//elimina nombres resptidos en directores
+export const filteredDirector = function(movies) {
+  const onlyDirectorName = movies.filter((director, index) => {
+    return movies.indexOf(director) === index
   })
   return onlyDirectorName
 }
 
-export const calculate = function(movies) {
+export const mappedTitle = function(movies) {
+  const titles = movies.map(anyArray => anyArray.title)
+  return titles
+}
+
+export const calculate = function(movies) {  
+  const moviesGender = [];
   for (let i=0; i<movies.length; i++){
     const movie = movies[i];
     let femaleCount = 0;
     let maleCount = 0;
+    const movieGender = [];  
     for (let j=0; j<movie.people.length; j++) {
       const person = movie.people[j];
       if (person.gender === 'Female'){
@@ -35,7 +43,14 @@ export const calculate = function(movies) {
         maleCount += 1;
       }
     }
-    console.log(femaleCount);
-    console.log(maleCount);
+    movieGender.push(femaleCount);
+    movieGender.push(maleCount);
+    moviesGender.push(movieGender);       
   }
+  
+  //console.log(moviesTitleGender);
+  return moviesGender
 }
+
+  
+
--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 
-import { alphabeticOrderAsc, alphabeticOrderDes, filteredDirector, mappedDirector } from "./data.js";
+
+import { alphabeticOrderAsc, alphabeticOrderDes, filteredDirector, mappedDirector, calculate, mappedTitle } from "./data.js";
+
 import data from "./data/ghibli/ghibli.js";
 
 const movies = data.films;
@@ -9,19 +11,57 @@ const root = document.getElementById("root");
 
 const titleImage = (movies) => {
   for (const movie of movies) {
-    // const movieTitle = movie.title;
     const movieDiv = document.createElement("div");
-    movieDiv.classList.add("card"); //clase para editar en css
-    //con movie.title llamamos a la constante y le asignamos una etiqueta
-    movieDiv.innerHTML += ` <img src="${movie.poster}" alt="imágen de la película"><h2>${movie.title}</h2><div id="cuadro"></div><h3>Director:${movie.director}</h3>
-    <p>Release Date:<br>${movie.release_date}</p><p1>Rate:<br> ${movie.rt_score}</p1><p2>${movie.description}</p2>`//siempre poner en imágenes alt
+    movieDiv.classList.add("card");
+    movieDiv.innerHTML += `
+      <img src="${movie.poster}" alt="imágen de la película">
+      <h2>${movie.title}</h2>
+      <dialog class="modal" id="modal-${movie.id}">
+        <div id="cuadro"></div>
+        <h3>Director: ${movie.director}</h3>
+        <p>Release Date:  ${movie.release_date}</p>
+        <p>Rate:  ${movie.rt_score}</p>
+        <p2>${movie.description}</p2>
+        <div id="charactersgender"></div>
+        <button class="close"><a href="index.html">volver</a></button>
+      </dialog>
+    `;
     root.appendChild(movieDiv);
 
+    const modal = document.querySelector(`#modal-${movie.id}`);
+    movieDiv.addEventListener("click", () => {
+      modal.showModal();
+    });
+    const movieGender = movieDiv.querySelector("#charactersgender");
+    const titles = mappedTitle(movies);
+    const moviesGender = calculate(movies);
+    const moviesTitleGender = [];
+    for (let i = 0; i < movies.length; i++){    
+      moviesTitleGender.push({
+        name: titles[i],
+        female: moviesGender[i][0],
+        male: moviesGender[i][1]
+      })      
+    }
+    const showGender = () => {
+      for (let i = 0; i < moviesTitleGender.length; i++){
+        const movieName = movieDiv.querySelector("h2").textContent;
+        if ( movieName === moviesTitleGender[i].name){
+          const femaleGender = moviesTitleGender[i].female;
+          const maleGender = moviesTitleGender[i].male;
+          const genders = document.createElement('h4');
+          genders.textContent = `Female: ${femaleGender} Male: ${maleGender}`;
+          movieGender.appendChild(genders);            
+        }        
+      }
+    }
+    showGender(movies);
   }
 };
 titleImage(movies);
 
-const copyMovies = data.films;
+const copyMovies = structuredClone(movies);
+
 const orderBy = (copyMovies) => {
   const buttonSelection = document.getElementById("order")
   buttonSelection.addEventListener("change", () => {
@@ -31,13 +71,17 @@ const orderBy = (copyMovies) => {
       const orderAZ = alphabeticOrderAsc(copyMovies);    
       titleImage(orderAZ);
     }
-    if (selectedOption === "z-a"){
+    else if (selectedOption === "z-a"){
       root.innerHTML = "";
       const orderZA = alphabeticOrderDes(copyMovies);
       titleImage(orderZA);
     }
-  });  
-};
+    else if (selectedOption === "default"){
+      root.innerHTML = "";
+      titleImage(movies);      
+    }
+  });
+}
 orderBy(copyMovies);
 
 const director = mappedDirector(movies);
@@ -96,3 +140,25 @@ document.getElementById("directors").addEventListener("change", function(){
   titleImage(directorFilter);
 });
 
+const titles = mappedTitle(movies);
+const moviesGender = calculate(movies);
+const moviesTitleGender = [];
+for (let i = 0; i < movies.length; i++){
+  moviesTitleGender.push({
+    name: titles[i],
+    female: moviesGender[i][0],
+    male: moviesGender[i][1]
+  })
+}
+console.log(moviesTitleGender);
+
+const elementGender = document.getElementById("charactersgender");
+for (let i = 0; i < moviesTitleGender.length; i++){
+  if (movies.title === moviesTitleGender[i].name){
+    const femaleGender = moviesTitleGender[i].female;
+    const maleGender = moviesTitleGender[i].male;
+    const genders = document.createElement('p');
+    genders.innerHTML = "Female: " + femaleGender + "  " + "Male: " + maleGender;
+    elementGender.appendChild(genders);
+  }  
+}
