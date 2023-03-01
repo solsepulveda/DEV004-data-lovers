17275124adb21b7e6a15abfe5b196aeded7cb219
solsepulveda <solangesepulved@gmail.com>
2023-03-01 20:24:25 -0300

    cierre de modal

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,44 +1,43 @@
-import { alphabeticOrderAsc,alphabeticOrderDes,mappedDirector,filteredDirector } from '../src/data.js';
+import { alphabeticOrderAsc, alphabeticOrderDes, mappedDirector, filteredDirector, mappedTitle, calculate} from '../src/data.js';
 
-describe('alphabeticOrderAsc', () => {
+
+describe('alphabeticOrderAsc' , () => {
   it('is a function', () => {
     expect(typeof alphabeticOrderAsc).toBe('function');
-  });
+  })
 
-  const data = [{"title": "Castle in the Sky"},{"title": "Kiki's Delivery Service"},{"title": "Grave of the Fireflies"}];
+  const  movies = [{"id": 1, "title":'uno', "generos": ['accion','terror']},
+    {"id": 2, "title":'dos', "generos": ['terror']},
+    {"id":3, "title":'tres', "generos": ['accion', 'comedia']}];
 
-  it('sorts data in ascending alphabetic order', () => {
-    const expected = [{"title": "Castle in the Sky"},{"title": "Grave of the Fireflies"},{"title": "Kiki's Delivery Service"}];
-    const result = alphabeticOrderAsc(data);
-    expect(result).toEqual(expected);
-  });
-});
+  it('alphabeticOrderAsc', () => {
+    expect(alphabeticOrderAsc(movies)).toStrictEqual([{"id": 2, "title":'dos', "generos": ['terror']}, {"id":3, "title":'tres', "generos": ['accion', 'comedia']}, {"id": 1, "title":'uno', "generos": ['accion','terror']}])
+  })  
+})
+
 
 describe('alphabeticOrderDes', () => {
   it('is a function', () => {
     expect(typeof alphabeticOrderDes).toBe('function');
-  });
-
-  const  peliculas = [{"id": 1, "title":'uno', "generos": ['accion','terror']},
-    {"id": 2, "title":'dos', "generos": ['terror']},
-    {"id":3, "title":'tres', "generos": ['accion', 'comedia']}]
-
-  it('alphabeticOrderDes', () => {
-    expect(alphabeticOrderDes(peliculas)).toStrictEqual([{"id": 1, "title":'uno', "generos": ['accion','terror']}, {"id":3, "title":'tres', "generos": ['accion', 'comedia']}, {"id": 2, "title":'dos', "generos": ['terror']}]);
-  });
-});
-
-describe('mappedDirector', () => {
-  it ('is a function', () => {    
-    expect(typeof mappedDirector).toBe('function');
   })
 
-  const  peliculas = [{"id": 1, "title":'uno', "generos": ['accion','terror'], "director": "Juan Perez"},
+  const  movies = [{"id": 1, "title":'uno', "generos": ['accion','terror']},
+    {"id": 2, "title":'dos', "generos": ['terror']},
+    {"id":3, "title":'tres', "generos": ['accion', 'comedia']}]
+  it('alphabeticOrderDes', () => {
+    expect(alphabeticOrderDes(movies)).toStrictEqual([{"id": 1, "title":'uno', "generos": ['accion','terror']}, {"id":3, "title":'tres', "generos": ['accion', 'comedia']}, {"id": 2, "title":'dos', "generos": ['terror']}])
+  })
+})
+
+describe('mappedDirector', () => {
+  it ('is a function', () => {
+    expect(typeof mappedDirector).toBe('function');
+  })
+  const  movies = [{"id": 1, "title":'uno', "generos": ['accion','terror'], "director": "Juan Perez"},
     {"id": 2, "title":'dos', "generos": ['terror'], "director": "Wes Anderson"},
     {"id": 3, "title":'tres', "generos": ['accion', 'comedia'], "director": "Guillermo Totoro"}]
-
   it('mappedDirector', () => {
-    expect(mappedDirector(peliculas)).toEqual(["Juan Perez", "Wes Anderson", "Guillermo Totoro"])
+    expect(mappedDirector(movies)).toEqual(["Juan Perez", "Wes Anderson", "Guillermo Totoro"])
   })
 })
 
@@ -46,13 +45,43 @@ describe("filteredDirector",() =>{
   it("is a function", () => {
     expect(typeof filteredDirector).toBe("function");
   });
-
+  
   const data = ["Hayao Miyazaki", "Hiromasa Yonebayashi", "Isao Takahata", "Hayao Miyazaki", "Hiromasa Yonebayashi", "Isao Takahata"];
   it("filteredDirector",() =>{
-
     const expected = ["Hayao Miyazaki", "Hiromasa Yonebayashi", "Isao Takahata"];
     const result = filteredDirector(data);
     expect(result).toEqual(expected);
   })
 });
 
+describe('mappedTitle', () => {
+  it ('is a function', () => {
+    expect(typeof mappedTitle).toBe('function');
+  })
+  const  movies = [{"id": 1, "title":'uno', "generos": ['accion','terror'], "director": "Juan Perez"},
+    {"id": 2, "title":'dos', "generos": ['terror'], "director": "Wes Anderson"},
+    {"id": 3, "title":'tres', "generos": ['accion', 'comedia'], "director": "Guillermo Totoro"}]
+  it('mappedTitle', () => {
+    expect(mappedTitle(movies)).toEqual(['uno', 'dos', 'tres'])
+  })
+})
+
+describe('calculate', () => {
+  it ('is a function', () => {
+    expect(typeof calculate).toBe('function');
+  })
+
+  const movies = [{"id": 1, "title":'uno', "generos": ['accion','terror'], "people": [{
+    "name": "Pazu","gender": "Male", "specie": "Human"}, {
+    "name": "Satsuki Kusakabe","gender": "Female","specie": "Human"}] },
+  {"id": 2, "title":'dos', "generos": ['terror'], "people": [{
+    "name": "Pazu","gender": "Male", "specie": "Human"}, {
+    "name": "Satsuki Kusakabe","gender": "Male","specie": "Human"}] },
+  {"id":3, "title":'tres', "generos": ['accion', 'comedia'], "people": [{
+    "name": "Pazu","gender": "Female", "specie": "Human"}, {
+    "name": "Satsuki Kusakabe","gender": "Female","specie": "Human"}]}]
+
+  it('calculate', () => {
+    expect(calculate(movies)).toEqual([[1,1], [0,2], [2,0]])
+  })
+})
